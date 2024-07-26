49c30024419166989bc4f9953cdbbb49f987e5bd
ChukaUSW <chuka.nwankwo2@gmail.com>
2024-07-26 17:55:20 +0100

    Changed ejs instead of handlebars

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const handlebars = require("express-handlebars");
+const ejsLayout = require("express-ejs-layouts");
 
 const app = express();
 
@@ -8,23 +8,26 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static(path.resolve("public")));
 
-//Sets our app to use the handlebars engine
-app.engine("handlebars", handlebars.engine());
-app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(ejsLayout);
+app.set("layout", "layouts/layout");
+
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // Routes
 app.get("/", (req, res) => {
-  res.render("index", { layout: "index" });
+  res.render("index");
 });
 app.get("/products", (req, res) => {
-  res.render("products", { layout: "index" });
+  res.render("products");
 });
 app.get("/cart", (req, res) => {
-  res.render("cart", { layout: "index" });
+  res.render("cart");
 });
 app.get("/checkout", (req, res) => {
-  res.render("checkout", { layout: "index" });
+  res.render("checkout");
 });
 
 app.listen(port, () => console.log("Server listening to port: " + port));
