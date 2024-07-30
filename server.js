e5df131c3d5c3fb41cd0748cd9ae79fab9f0dd3c
ChukaUSW <chuka.nwankwo2@gmail.com>
2024-07-30 18:21:58 +0100

    Cards are coming from api

--- server.js
+++ server.js
@@ -16,12 +16,39 @@ app.set("layout", "layouts/layout");
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
 
+const products = [
+  {
+    id: 1,
+    title: "Mens Shoes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam illo quia nemo voluptatem doloremque, ipsam nesciunt.",
+    price: 20,
+    image: "/img/product-demo.jpeg",
+  },
+  {
+    id: 2,
+    title: "Polo Shirt",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam illo quia nemo voluptatem doloremque, ipsam nesciunt.",
+    price: 10,
+    image: "/img/product-demo.jpeg",
+  },
+  {
+    id: 3,
+    title: "USB Stick",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam illo quia nemo voluptatem doloremque, ipsam nesciunt.",
+    price: 15,
+    image: "/img/product-demo.jpeg",
+  },
+];
+
 // Routes
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { products });
 });
 app.get("/products", (req, res) => {
-  res.render("products");
+  res.render("products", { products });
 });
 app.get("/cart", (req, res) => {
   res.render("cart");
